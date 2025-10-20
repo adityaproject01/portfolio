89a04ce4d474779dd9c0717970e6ed416b0b01c5
adityaproject01 <aditya31031998@gmail.com>
2025-10-21 00:46:44 +0530

    s

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import calimg from "./img/images/calculator.png";
 import todoimg from "./img/images/todo1.png";
 import weatherImg from "./img/images/weather.jpg";
+import ecommImg from "./img/images/ecommer.jpg";
 const projects = [
   
   {
     title: "Ecommerce Dashboard",
     description:
-      "Real-time monitoring dashboard for CI/CD pipelines and server metrics.",
-    tech: ["React", "Docker", "AWS"],
+      "Smooth shopping experience with product filtering and order tracking Your one-stop shop for electronics, fashion, and more. Designed for both desktop and mobile users.",
+    tech: ["React", "NodeJs", "ExpressJs"],
     colorFrom: "from-pink-500",
     colorTo: "to-blue-600",
-    iconPath:
-      "M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M12,6A6,6 0 0,0 6,12A6,6 0 0,0 12,18A6,6 0 0,0 18,12A6,6 0 0,0 12,6M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8Z",
-    border: "border-pink-400",
+    iconPath:ecommImg,
+      border: "border-pink-400",
     link: "/ecommerce",
   },
   {
